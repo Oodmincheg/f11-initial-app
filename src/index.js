eceecd194351c9be364a393d58b0f15d8b77137e
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-11-17 22:05:26 +0200

    init react app

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Todo = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('p', {}, props.title),
+    React.createElement('p', {}, props.date),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('div', {}, [
+      React.createElement('div', {}, 'Your todos'),
+      React.createElement(Todo, { title: 'Learn react', date: '01-01-2022' }),
+      React.createElement(Todo, { title: 'Learn redux', date: '01-02-2022' }),
+      React.createElement(Todo, { title: 'Learn angular', date: '01-03-2056' }),
+    ]),
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
