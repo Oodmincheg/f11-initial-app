0135750e2461e5e38983bf7c4d7d7d60c965c0e1
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-11-22 20:04:40 +0200

    rewrite to jsx

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Todo = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('p', {}, props.title),
-    React.createElement('p', {}, props.date),
-  ]);
-};
+import Todo from './components/Todo';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('div', {}, [
-      React.createElement('div', {}, 'Your todos'),
-      React.createElement(Todo, { title: 'Learn react', date: '01-01-2022' }),
-      React.createElement(Todo, { title: 'Learn redux', date: '01-02-2022' }),
-      React.createElement(Todo, { title: 'Learn angular', date: '01-03-2056' }),
-    ]),
+  return (
+    <div>
+      <div>Your todos</div>
+      <Todo title="Learn react" date="01-01-2022" />
+      <Todo title="Learn redux" date="01-02-2022" />
+      <Todo title="Learn react" date="01-03-2023" />
+    </div>
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
