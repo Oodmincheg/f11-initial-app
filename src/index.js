5a32376be82013185e57007317742b5c45d6299c
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-11-22 21:54:13 +0200

    add todo form. add saving todo.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+
+// const todos = [
+//   { title: 'Learn react', date: '01-01-2022', id: 1 },
+//   { title: 'Learn redux', date: '01-02-2022', id: 2 },
+//   { title: 'Learn angular', date: '01-03-2022', id: 3 },
+// ];
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const saveTodo = (todo) => {
+    console.log(todo);
+    // todos.push(todo);
+    setTodos([...todos, todo]);
+  };
   return (
-    <div>
+    <>
+      <TodoForm saveTodo={saveTodo} />
       <div>Your todos</div>
-      <Todo title="Learn react" date="01-01-2022" />
+      {/* <Todo title="Learn react" date="01-01-2022" />
       <Todo title="Learn redux" date="01-02-2022" />
-      <Todo title="Learn react" date="01-03-2023" />
-    </div>
+      <Todo title="Learn react" date="01-03-2023" /> */}
+      {todos.map(({ title, description, priority }, index) => (
+        <Todo
+          title={title}
+          description={description}
+          priority={priority}
+          key={index}
+        />
+      ))}
+    </>
   );
 };
 
